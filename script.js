ff27ae5643c9752e8d98444926e644a3bc2e8c0c
HernandoVargasL <122489123+hernandovargaslopez@users.noreply.github.com>
2024-04-25 10:47:25 -0500

    khgkhg

--- script.js
+++ script.js
@@ -1,81 +1,81 @@
 document.addEventListener("DOMContentLoaded", function() {
     new BeerSlider(document.querySelector(".beer-slider"))
-    // var swiper1 = new Swiper('.swiper-herobanner', {
-    //     effect: 'fade',
-    //     fadeEffect: {
-    //         crossFade: true
-    //     },
-    //     pagination: {
-    //         el: '.swiper-pagination1',
-    //         clickable: true,
-    //     },
-    //     loop: true, // Enable loop mode
-    //     autoplay: {
-    //     delay: 7000, // Delay between transitions (in ms)
-    //     disableOnInteraction: false, // Stop autoplay on user interaction
-    //     },
-    //     on: {
-    //     init: function() {
-    //         var firstSlide = this.slides[0];
-    //         firstSlide.classList.add('zoomed');
-    //     },
-    //     slideChangeTransitionStart: function() {
-    //         var currentSlide = this.slides[this.activeIndex];
-    //         var zoomedSlide = this.slides[this.previousIndex];
+    var swiper1 = new Swiper('.swiper-herobanner', {
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        pagination: {
+            el: '.swiper-pagination1',
+            clickable: true,
+        },
+        loop: true, // Enable loop mode
+        autoplay: {
+        delay: 7000, // Delay between transitions (in ms)
+        disableOnInteraction: false, // Stop autoplay on user interaction
+        },
+        on: {
+        init: function() {
+            var firstSlide = this.slides[0];
+            firstSlide.classList.add('zoomed');
+        },
+        slideChangeTransitionStart: function() {
+            var currentSlide = this.slides[this.activeIndex];
+            var zoomedSlide = this.slides[this.previousIndex];
             
-    //         if (zoomedSlide) {
-    //         zoomedSlide.classList.remove('zoomed');
-    //         }
+            if (zoomedSlide) {
+            zoomedSlide.classList.remove('zoomed');
+            }
             
-    //         currentSlide.classList.add('zoomed');
-    //     }
-    //     }
-    // });  
+            currentSlide.classList.add('zoomed');
+        }
+        }
+    });  
 
-    // const swiper2 = new Swiper('#swiper2', {
-    //     // Optional parameters
-    //     direction: 'horizontal',
-    //     grid: {
-    //       rows: 2,
-    //     },
-    //     spaceBetween: 30,
-    //     loop: false,
+    const swiper2 = new Swiper('#swiper2', {
+        // Optional parameters
+        direction: 'horizontal',
+        grid: {
+          rows: 2,
+        },
+        spaceBetween: 30,
+        loop: false,
     
-    //     // Navigation arrows
-    //     navigation: {
-    //         nextEl: '.swiper-next',
-    //         prevEl: '.swiper-prev',
-    //     },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-next',
+            prevEl: '.swiper-prev',
+        },
     
-    //     // And if we need scrollbar
-    //     scrollbar: {
-    //         el: '.swiper-scrollbar',
-    //     },
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
 
-    //     // Pagination
-    //     pagination: {
-    //         el: '.swiper-pagination2',
-    //         clickable: true,
-    //     },
+        // Pagination
+        pagination: {
+            el: '.swiper-pagination2',
+            clickable: true,
+        },
 
-    //     // Responsive breakpoints
-    //     breakpoints: {
-    //         // when window width is <= 768px
-    //         1024: {
-    //             slidesPerView: 3,
-    //             spaceBetween: 20
-    //         },
-    //         768: {
-    //             slidesPerView: 2,
-    //             spaceBetween: 20
-    //         },
-    //         // when window width is <= 480px
-    //         510: {
-    //             slidesPerView: 1,
-    //             spaceBetween: 10
-    //         }
-    //     }
-    // });
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is <= 768px
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is <= 480px
+            510: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
     
     // const swiper3 = new Swiper('#swiper3', {
     //     // Optional parameters
