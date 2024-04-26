e503ef05c3606e5fd00b56f7c23e8893ce19d743
HernandoVargasL <122489123+hernandovargaslopez@users.noreply.github.com>
2024-04-26 10:17:09 -0500

    galerias

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    new BeerSlider(document.querySelector(".beer-slider"))
     var swiper1 = new Swiper('.swiper-herobanner', {
         effect: 'fade',
         fadeEffect: {
@@ -30,100 +29,71 @@ document.addEventListener("DOMContentLoaded", function() {
             currentSlide.classList.add('zoomed');
         }
         }
-    });  
-
+    });
+    
     const swiper2 = new Swiper('#swiper2', {
         // Optional parameters
         direction: 'horizontal',
-        grid: {
-          rows: 2,
-        },
+        slidesPerView: 1,
         spaceBetween: 30,
         loop: false,
     
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-next',
-            prevEl: '.swiper-prev',
-        },
-    
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-
         // Pagination
+        effect: 'cards',
         pagination: {
             el: '.swiper-pagination2',
             clickable: true,
         },
-
-        // Responsive breakpoints
-        breakpoints: {
-            // when window width is <= 768px
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-            // when window width is <= 480px
-            510: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            }
+    
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper2-next',
+            prevEl: '.swiper2-prev',
         }
     });
+
+    const swiper3 = new Swiper('#swiper3', {
+        // Optional parameters
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: false,
     
-    // const swiper3 = new Swiper('#swiper3', {
-    //     // Optional parameters
-    //     direction: 'horizontal',
-    //     slidesPerView: 1,
-    //     spaceBetween: 30,
-    //     loop: false,
+        // Pagination
+        effect: 'cards',
+        pagination: {
+            el: '.swiper-pagination3',
+            clickable: true,
+        },
     
-    //     // Pagination
-    //     pagination: {
-    //         el: '.swiper-pagination3',
-    //         clickable: true,
-    //     },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper3-next',
+            prevEl: '.swiper3-prev',
+        }
+    });
+
+    const swiper4 = new Swiper('#swiper4', {
+        // Optional parameters
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: false,
     
-    //     // Navigation arrows
-    //     navigation: {
-    //         nextEl: '.swiper2-next',
-    //         prevEl: '.swiper2-prev',
-    //     },
+        // Pagination
+        effect: 'cards',
+        pagination: {
+            el: '.swiper-pagination4',
+            clickable: true,
+        },
     
-    //     // And if we need scrollbar
-    //     scrollbar: {
-    //         el: '.swiper-scrollbar',
-    //     },
-    // });
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper4-next',
+            prevEl: '.swiper4-prev',
+        }
+    });
 
-    // const buttons = document.querySelectorAll(".wizard-buttons button");
-    // const steps = document.querySelectorAll(".step");
-
-    // buttons.forEach((button, index) => {
-    //     button.addEventListener("click", () => {
-    //         // Remove 'active' class from all buttons
-    //         buttons.forEach(btn => {
-    //             btn.classList.remove("active");
-    //         });
-
-    //         // Add 'active' class to the clicked button
-    //         button.classList.add("active");
-
-    //         // Hide all steps
-    //         steps.forEach(step => {
-    //             step.classList.remove("active");
-    //         });
-
-    //         // Show the corresponding step
-    //         steps[index].classList.add("active");
-    //     });
-    // });
 });
 
 
